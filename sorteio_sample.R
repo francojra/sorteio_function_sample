5ea94d1653dd2ca6df008f48fc80414ac744f937
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2021-10-18 00:03:25 -0300

    Add files via upload

--- sorteio_sample.R
+++ sorteio_sample.R
@@ -7,12 +7,18 @@
 x <- c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
 
 # Strings
-y <- c("João", "Maria", "Alice", "Eduardo")
+y <- c("João", "Maria", "Alice", "Eduardo", "Gabriel", "Elise")
 
-# Sorteio
+# Sorteios
 sample(x, 3, replace = T) # Sortear 3 números com repetição
 
 sample(x, 5, replace = T) # Sortear 5 números com repetição
 ### Observe que alguns números foram repetidos
 
 sample(x, 4, replace = F) # Sortear 4 números sem repetição
+
+sample(y, 2, replace = T) # Sortear 2 nomes com repetição
+
+sample(y, 5, replace = T) # Sortear 5 nomes sem repetição
+
+sample(y, 1) # Sortear 1 dos nomes
